0ffb05b59beae12219c6c8596bfedae8ee3d3e1d
Ostap21 <57452764+Ostap21@users.noreply.github.com>
2021-03-30 10:12:14 +0300

    Updates

---
+++ precache-manifest.5eb5e1305fa1e4c0f4c875e768a81f89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a7587c24074a89b52dff18d0c6ded8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "50b426a0e48eef7c5bf5",
+    "url": "./static/css/2.f6a27176.chunk.css"
+  },
+  {
+    "revision": "b8c07f45d3c7531f7a7c",
+    "url": "./static/css/main.a2f5eb59.chunk.css"
+  },
+  {
+    "revision": "50b426a0e48eef7c5bf5",
+    "url": "./static/js/2.0f427fe2.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "./static/js/2.0f427fe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8c07f45d3c7531f7a7c",
+    "url": "./static/js/main.d32a1eac.chunk.js"
+  },
+  {
+    "revision": "3cfb45b61c015a055433",
+    "url": "./static/js/runtime-main.0527da25.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2088c32903613710f8f9678a01b40cd4.js"
+  "./precache-manifest.5eb5e1305fa1e4c0f4c875e768a81f89.js"
 );
 
 self.addEventListener('message', (event) => {
